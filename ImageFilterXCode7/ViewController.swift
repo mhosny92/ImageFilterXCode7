97d3fa93f0e362c98e9b89f946e766b188b5d769
Mahmoud Hosny <mahmoud.h.abdallah@gmail.com>
2017-11-28 15:05:19 +0200

    Initial Commit

---
+++ ImageFilterXCode7/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageFilterXCode7
+//
+//  Created by Mahmoud Hosny on 11/28/17.
+//  Copyright © 2017 Mahmoud Hosny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
